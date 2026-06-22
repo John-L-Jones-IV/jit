4b95210404271588791a0cf1f6843222484d34b6
Johnny L Jones IV <johnny.l.jones.iv@gmail.com>
2026-06-21 20:11:33 -0400

    remove debug print statements

--- addJitToPath.sh
+++ addJitToPath.sh
@@ -3,7 +3,5 @@ parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd - P)
 cd "$parent_path"
 script_location="bin"
 jit_location="${PWD}/${script_location}"
-echo $jit_location
 export PATH="$PATH:$jit_location"
 echo ${PWD}
-PYTHONPATH="${PYTHONPATH}:${PWD}"
